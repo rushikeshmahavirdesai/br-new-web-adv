3c35daeff923427dffe56cd7da2cdea74fe908d4
rushikeshmahavirdesai <rushikeshmahavirdesai@gmail.com>
2023-05-29 23:43:36 +0530

    GAP ADDED

--- src/views/components/keyFeatures.vue
+++ src/views/components/keyFeatures.vue
@@ -3,11 +3,11 @@
     <!-- <div class="font-devanagari text-[#347571]  font-bold text-2xl m-5 text-center">
       भारतरथ का World Class Software, भारत के Businesses के लिए
     </div> -->
-    <Title :des="'भारत के Businesses के लिए'" :title="'भारतरथ का World Class Softwar'" />
+    <Title :des="'भारत के Businesses के लिए'" :title="'भारतरथ का World Class Software'" />
     <section class="features">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
-        <div class="bg-[#f7fdf3] flex flex-col items-center w-full">
-          <img src="../../assets/features/website.png" alt="" class="p-5" />
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <div class="bg-[#f7fdf3] flex flex-col items-center w-full justify-between">
+          <img src="../../assets/features/website.png" alt="" class="p-5 !h-48" />
           <div class="text-xl font-bold">e-Commerce Website</div>
           <div class="text-center p-5">
             मिनटों में अपना खुद का e-Commerce business start कीजिये. Digital India के इस दौर में, अब
@@ -15,8 +15,8 @@
           </div>
           <a href="#e-Commerce_Website"><img src="../../assets/features/down-button.png" alt="" class="pt-2 pb-5" /></a>
         </div>
-        <div class="bg-[#ecf8e5] flex flex-col items-center w-full">
-          <img src="../../assets/features/billing.png" alt="" class="p-5" />
+        <div class="bg-[#ecf8e5] flex flex-col items-center w-full justify-between">
+          <img src="../../assets/features/billing.png" alt="" class="p-5 !h-48" />
           <div class="text-xl font-bold">GST Billing System</div>
           <div class="text-center p-5">
             आपकी Shop हो या Warehouse, Distributorship हो या Production unit, भारतरथ आपके लिए लाये
@@ -24,8 +24,8 @@
           </div>
           <a href="#Inventory_Management"><img src="../../assets/features/down-button.png" alt="" class="pt-2 pb-5" /></a>
         </div>
-        <div class="bg-[#dcf4ce] flex flex-col items-center w-full">
-          <img src="../../assets/features/inventory.png" alt="" class="p-5" />
+        <div class="bg-[#dcf4ce] flex flex-col items-center w-full justify-between">
+          <img src="../../assets/features/inventory.png" alt="" class="p-5 !h-48" />
           <div class="text-xl font-bold">Inventory Management</div>
           <div class="text-center p-5">
             एक ही Integrated Inventory से आपके सभी Sales Channels (e-Commerce, Shop, B2B, Whatsapp)
